51de50781fa71740d43b5844530ac87282c885a1
Saumy <saumy2502@gmail.com>
2025-09-29 20:26:31 +0530

    feat: Add morgan for HTTP request logging

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const morgan = require('morgan');
 const { Pool } = require("pg");
 
 const app = express();
@@ -14,6 +15,7 @@ const pool = new Pool({
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.get("/", (req, res) => {
   res.json({ message: "Server is running" });
